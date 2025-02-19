647a110ee6c1111c777b312da4b57acacd98933b
galinaB-95 <galinabilokur@gmail.com>
2025-02-19 15:31:30 +0300

    Initial commit

---
+++ src/main/java/org/example/FirstClass.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+        printThreeWords();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+}
+
